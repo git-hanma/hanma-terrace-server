f6d08132e62c50218f130fae23586ae0495bc199
wh <942464176@qq.com>
2019-07-13 10:27:01 +0800

    第一次提交 王辉

---
+++ src/main/java/com/hanma/HanmaTerraceServerApplication.java
@@ -0,0 +1,13 @@
+package com.hanma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanmaTerraceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanmaTerraceServerApplication.class, args);
+	}
+
+}
