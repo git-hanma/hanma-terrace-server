c46730b9362875434e89d08e02e04a69c588d029
shenxiaoyan <279700467@qq.com>
2019-07-20 15:02:54 +0800

    更新

--- src/main/java/com/terrace/mapper/TerraceMapper.java
+++ src/main/java/com/terrace/mapper/TerraceMapper.java
@@ -2,12 +2,14 @@ package com.terrace.mapper;
 
 import com.terrace.domain.TerraceVO;
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.Mapping;
 import java.util.List;
 
 /**
  * Created by 小妞 on 2019/7/16.
  */
+@Repository
 @Mapper
 public interface TerraceMapper {
     List<TerraceVO> userListYh();
